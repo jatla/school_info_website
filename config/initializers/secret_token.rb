077d19cadc62acf6bcb9a8dcc3a88ea3e6effab2
JayaPrakash Reddy Atla <jatla@Sunithas-MacBook-Air.local>
2014-04-10 16:22:31 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SchoolInfoWebsite::Application.config.secret_key_base = 'bb724e74870ff5818a495fd8470691e5f06841819a90066f4b16a3790222c9859a3882c10c699e0f85ff652b7c1261963a39901213e4479989e7ae8bb9469d14'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SchoolInfoWebsite::Application.config.session_store :cookie_store, key: '_school_info_website_session'
